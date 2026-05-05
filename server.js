03056cb850621f2d420353ce8665dd57441048d3
Yasin <yasinhacker135@gmail.com>
2026-05-05 13:31:40 +0700

    update

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const app = express()
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://toko-kopi-bubuk.vercel.app/', // ← ganti dengan URL Vercel kamu
+    'https://toko-kopi-bubuk.vercel.app', // ← ganti dengan URL Vercel kamu
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
